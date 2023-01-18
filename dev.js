e6fc279a122127f945092aaa7506c9acb5122bc4
Mattias Persson <mattias.persson.87@gmail.com>
2023-01-18 03:30:30 +0100

    add local dev file

---
+++ dev.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name         better-feber
+// @version      dev
+// @description  enhancements for feber.se
+// @match        https://feber.se/*
+// @grant        none
+// @run-at       document-start
+// @require      file:///Users/matte/Developer/better-feber/better-feber.js
+// ==/UserScript==
